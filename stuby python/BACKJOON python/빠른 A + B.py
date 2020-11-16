cf34ce7c709e2b75bad2c2e7b0c4f29b9b2cb3ee
minecode0606 <minseo_0606@naver.com>
2020-11-16 23:27:37 +0900

    BACKJOON Algorithm Study File Saves

--- stuby python/BACKJOON python/빠른 A + B.py
+++ stuby python/BACKJOON python/빠른 A + B.py
@@ -1,10 +1,8 @@
-import sys
 add01 = 0
 test_case = int(input(""))
 for _ in range(test_case):
-    input01 = sys.stdin.readline()
-    input02 = input01.split()
-    list01 = list(input02)
+    input01 = input().split()
+    list01 = list(input01)
     for i in list01:
         add01 += int(i)
     print(add01)
